e2ebbdc0358ff2dcef8e739af3aa2218596840dc
Delor Rotstrump <delor@delor.home>
2019-01-14 19:50:36 +0100

    db seeding 2

--- libraryServer/database/seeds/UsersTableSeeder.php
+++ libraryServer/database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +12,25 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'bibl',
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+            'role' => 1,
+        ]);
+        DB::table('users')->insert([
+            'name' => 'user1',
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+            'role' => 2,
+        ]);
+        for ($i = 0; $i < 8; $i++) {
+            DB::table('users')->insert([
+                'name' => str_random(10),
+                'email' => str_random(10).'@gmail.com',
+                'password' => bcrypt('secret'),
+                'role' => 2,
+            ]);
+        }
     }
 }
