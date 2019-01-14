bda1118aa93c39fff91215591418109ecf10fddf
Delor Rotstrump <delor@delor.home>
2019-01-14 20:47:46 +0100

    db seeding ...

--- libraryServer/database/seeds/UsersTableSeeder.php
+++ libraryServer/database/seeds/UsersTableSeeder.php
@@ -12,18 +12,21 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        //librarian
         DB::table('users')->insert([
             'name' => 'bibl',
             'email' => str_random(10).'@gmail.com',
             'password' => bcrypt('secret'),
             'role' => 1,
         ]);
+        //first user
         DB::table('users')->insert([
             'name' => 'user1',
             'email' => str_random(10).'@gmail.com',
             'password' => bcrypt('secret'),
             'role' => 2,
         ]);
+        //rest of users
         for ($i = 0; $i < 8; $i++) {
             DB::table('users')->insert([
                 'name' => str_random(10),
