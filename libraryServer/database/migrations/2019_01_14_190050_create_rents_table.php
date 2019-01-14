8013d3e3a29a94f00c0a131504f305aae067410c
Delor Rotstrump <delor@delor.home>
2019-01-14 20:06:03 +0100

    db seeding 3

---
+++ libraryServer/database/migrations/2019_01_14_190050_create_rents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rents', function (Blueprint $table) {
+            $table->increments('idrent');
+            $table->integer('iduser');
+            $table->integer('idbook');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rents');
+    }
+}
