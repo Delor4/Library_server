9b8d4580eb26a395be9633476d20afee59981987
Delor Rotstrump <delor@delor.home>
2019-01-14 19:25:51 +0100

    starting with db seeding

--- libraryServer/database/seeds/DatabaseSeeder.php
+++ libraryServer/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            BooksTableSeeder::class,
+            CatalogTableSeeder::class,
+            ReservationsTableSeeder::class,
+            RentsTableSeeder::class,
+            LogTableSeeder::class
+        ]);
     }
 }
